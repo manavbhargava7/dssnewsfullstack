6167c89dfe94e3efed4a78994ce5c73814780303
Alicia <awwang629@gmail.com>
2024-10-30 14:36:16 -0700

    changed threecolslider layout

--- treact_template/src/components/cards/ThreeColSlider.js
+++ treact_template/src/components/cards/ThreeColSlider.js
@@ -88,7 +88,7 @@ const PrimaryButton = styled(PrimaryButtonBase)`
   }
 `;
 
-export default () => {
+export default ({ cards, title }) => {
   // useState is used instead of useRef below because we want to re-render when sliderRef becomes available (not null)
   const [sliderRef, setSliderRef] = useState(null);
   const sliderSettings = {
@@ -111,82 +111,7 @@ export default () => {
     ]
   };
 
-  /* Change this according to your needs */
-  const cards = [
-    {
-      imageSrc: "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-      title: "Wyatt Residency",
-      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-      locationText: "Rome, Italy",
-      pricingText: "USD 39/Day",
-      rating: "4.8",
-    },
-    {
-      imageSrc: "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-      title: "Soho Paradise",
-      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-      locationText: "Ibiza, Spain",
-      pricingText: "USD 50/Day",
-      rating: 4.9,
-    },
-    {
-      imageSrc: "https://images.unsplash.com/photo-1549294413-26f195200c16?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-      title: "Hotel Baja",
-      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-      locationText: "Palo Alto, CA",
-      pricingText: "USD 19/Day",
-      rating: "5.0",
-    },
-    {
-      imageSrc: "https://images.unsplash.com/photo-1571770095004-6b61b1cf308a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=1024&w=768&q=80",
-      title: "Hudak Homes",
-      description: "Lorem ipsum dolor sit amet, consectur dolori adipiscing elit, sed do eiusmod tempor nova incididunt ut labore et dolore magna aliqua.",
-      locationText: "Arizona, RAK",
-      pricingText: "USD 99/Day",
-      rating: 4.5,
-    },
-  ]
-
-  const trendingCards = [
-    {
-      imageSrc: "https://eu-images.contentstack.com/v3/assets/blt6b0f74e5591baa03/blt04eeba92b0e5bb7a/671ad1e1d46cff7e25ac0803/GettyImages-2149059417.jpg?width=1280&auto=webp&quality=95&format=jpg&disable=upscale",
-      title: "IBM Unveils Granite 3.0",
-      description: "IBM is diving deeper into the large language model race with Granite 3.0.",
-      topicText: "Artificial Intelligence",
-      authorText: "Christopher Hutton",
-      rating: "4.8",
-      url: "https://aibusiness.com/nlp/ibm-unveils-granite-3-0-as-workhorse-ai-model"
-    },
-    {
-      imageSrc: "https://eu-images.contentstack.com/v3/assets/blt6b0f74e5591baa03/blt981df99721245dc6/671aa04c0aa6043d1d9a10ef/GettyImages-693893643.jpg?width=1280&auto=webp&quality=95&format=jpg&disable=upscale",
-      title: "Nvidia, Microsoft Join Forces",
-      description: "Nvidia, Microsoft Join Forces to Accelerate AI Health Care Startups. Microsoft provides $350,000 worth of software tools to each company.",
-      topicText: "AI Healthcare",
-      authorText: "Heidi Vella",
-      rating: 4.9,
-      url: "https://aibusiness.com/nlp/nvidia-microsoft-join-forces-to-accelerate-ai-health-care-startups"
-    },
-    {
-      imageSrc: "https://www.datasciencecentral.com/wp-content/uploads/2024/10/why-ai-bias-is-a-cybersecurity-risk-feature-scaled.jpg",
-      title: "Why AI bias is a cybersecurity risk",
-      description: "Artificial intelligence (AI) has made its way into nearly every facet of running a small or mid-sized business in the modern age. When programmed appropriately, AI can improve response time and catch security threats before they become a problem. Unfortunately, AI inherently comes with the potential for biases and can skew algorithms in strange ways.",
-      topicText: "AI Security",
-      authorText: "Zachary Amos",
-      rating: "5.0",
-      url: "https://www.datasciencecentral.com/why-ai-bias-is-a-cybersecurity-risk-and-how-to-address-it/"
-    },
-    {
-      imageSrc: "https://cdn.prod.website-files.com/63ccf2f0ea97be12ead278ed/644a18b637053fa3709c5ba2_what-is-data-science.jpg",
-      title: "4 Years of Data Science in 8 Minutes",
-      description: "What I have learned in my 4+ year journey of studying data science.",
-      topicText: "Data Science",
-      authorText: "Egor Howell",
-      rating: 4.5,
-      url: "https://towardsdatascience.com/4-years-of-data-science-in-8-minutes-6ea5b10f0192"
-    },
-  ]
-
-  const [likes, setLikes] = useState(Array(trendingCards.length).fill(false));
+  const [likes, setLikes] = useState(Array(cards.length).fill(false));
 
   const toggleLike = (index) => {
     setLikes((prevLikes) =>
@@ -198,7 +123,7 @@ export default () => {
     <Container>
       <Content>
         <HeadingWithControl>
-          <Heading>Popular Hotels</Heading>
+          <Heading>{title}</Heading>
           <Controls>
             <PrevButton onClick={sliderRef?.slickPrev}><ChevronLeftIcon /></PrevButton>
             <NextButton onClick={sliderRef?.slickNext}><ChevronRightIcon /></NextButton>
@@ -206,48 +131,6 @@ export default () => {
         </HeadingWithControl>
         <CardSlider ref={setSliderRef} {...sliderSettings}>
           {cards.map((card, index) => (
-            <Card key={index}>
-              <CardImage imageSrc={card.imageSrc} />
-              <TextInfo>
-                <TitleReviewContainer>
-                  <Title>{card.title}</Title>
-                  <RatingsInfo>
-                    <StarIcon />
-                    <Rating>{card.rating}</Rating>
-                  </RatingsInfo>
-                </TitleReviewContainer>
-                <SecondaryInfoContainer>
-                  <IconWithText>
-                    <IconContainer>
-                      <LocationIcon />
-                    </IconContainer>
-                    <Text>{card.locationText}</Text>
-                  </IconWithText>
-                  <IconWithText>
-                    <IconContainer>
-                      <PriceIcon />
-                    </IconContainer>
-                    <Text>{card.pricingText}</Text>
-                  </IconWithText>
-                </SecondaryInfoContainer>
-                <Description>{card.description}</Description>
-              </TextInfo>
-              <PrimaryButton>Book Now</PrimaryButton>
-            </Card>
-          ))}
-        </CardSlider>
-      </Content>
-
-      <Content>
-        <HeadingWithControl>
-          <Heading>Trending</Heading>
-          <Controls>
-            <PrevButton onClick={sliderRef?.slickPrev}><ChevronLeftIcon /></PrevButton>
-            <NextButton onClick={sliderRef?.slickNext}><ChevronRightIcon /></NextButton>
-          </Controls>
-        </HeadingWithControl>
-        <CardSlider ref={setSliderRef} {...sliderSettings}>
-          {trendingCards.map((card, index) => (
             <Card key={index}>
               <CardImage imageSrc={card.imageSrc} />
               <TextInfo>
